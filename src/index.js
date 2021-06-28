e8fae13fed6893a79fbd8fbc90e356c9a994b0a5
Neo.Eskini <mreskini30@gmail.com>
2021-06-29 02:09:34 +0430

    installed the dependencies

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from "./app.jsx"
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
