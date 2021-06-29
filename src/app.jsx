643bfa94a5d2f2798392cf9321752e5f745f22d9
Neo.Eskini <mreskini30@gmail.com>
2021-06-29 17:21:49 +0430

    completed the accomplishments section for the desktop view

--- src/app.jsx
+++ src/app.jsx
@@ -3,6 +3,7 @@ import Hero from "./components/hero"
 import Projects from "./components/projects"
 import Technologies from "./components/technologies"
 import AboutMe from "./components/about-me"
+import Accomplishments from "./components/accomplishments"
 function App() {
   return (
     <div>
@@ -11,6 +12,7 @@ function App() {
       <Projects />
       <Technologies />
       <AboutMe />
+      <Accomplishments />
     </div>
   )
 }
---
+++ src/components/accomplishments.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react"
+
+export default function Accomplishments(){
+    const [cards, setCards] = useState([
+        {
+            title: "20+",
+            description: "Open Source Projects",
+        },
+        {
+            title: "1000+",
+            description: "Students",
+        },
+        {
+            title: "1900+",
+            description: "Github Followers",
+        },
+    ])
+    const createAccomplishmentsCardTemplate = (title, description) =>{
+        return(
+            <>
+                <div className="col-lg-3 p-4 bg-secondary-color text-center shadow-light rounded">
+                    <div className="text-light h2">
+                        {title}
+                    </div>
+                    <div className="text-light h4 font-weight-light mt-4">
+                        {description}
+                    </div>
+                </div>
+                <div className="col-lg-1"></div>
+            </>
+        )
+    }
+    return(
+        <div className="container-fluid position-relative">
+            <div className="row m-0">
+                <div className="col-lg-10 mx-auto my-5 py-3">
+                    <div className="display-4 text-light mb-4">
+                        <div className="hr-line mb-3"></div>
+                        •
+                        Accomplishments
+                    </div>
+                    <div className="mt-5 row m-0">
+                    {
+                        cards.map(card => createAccomplishmentsCardTemplate(card.title, card.description))
+                    }
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
