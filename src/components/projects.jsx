3669706e5d9690bfdae506b6567c3fd6d75eb4c0
Neo.Eskini <mreskini30@gmail.com>
2021-06-29 14:10:01 +0430

    implemented the projects section with desktop styles only

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,4 +1,60 @@
+import { useState } from "react"
+import image1 from "../assets/images/sample-1.png"
+import image2 from "../assets/images/sample-2.png"
+import image3 from "../assets/images/sample-3.jpg"
+import image4 from "../assets/images/sample-4.jpg"
 export default function Projects(){
+    const [samples, setSamples] = useState([
+        {
+            title: "MERN Memories",
+            description: "Using React, Node.js, Express & MongoDB you'll learn how to build a Full Stack MERN Application - from start to finish. The App is called Memories and it is a simple social media app that allows users to post interesting events that happened in their lives.",
+            image: image1,
+        },
+        {
+            title: "E-Commerce",
+            description: "While building it you'reStripe for card transactions. On top of that, at the end of the video, you will have this unique and complex webshop app that you will be able to add to your portfolio. And trust me, e-commerce applications are impressive.",
+            image: image2,
+        },
+        {
+            title: "WebRTC App",
+            description: "This is a code repository for the corresponding YouTube video. In this tutorial, we're going to build and deploy a React Video Chat Application using WebRTC.",
+            image: image3,
+        },
+        {
+            title: "Unichat",
+            description: "This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application",
+            image: image4,
+        },
+    ])
+    const createPortfolioCardTemplate = (title, description, image) => {
+        return(
+            <div className="col-lg-6 p-5">
+                <div className="shadow-light">
+                    <img src={image} alt="Sample" style={{maxHeight: "300px"}} className="img-fluid rounded h-100" />
+                    <div className="text-light h2 font-weight-light p-3">
+                        {title}
+                    </div>
+                    <div className="p-3 text-secondary">
+                        {description}
+                    </div>
+                    <div className="list-inline mx-auto pb-4 py-2 text-light text-center font-weight-light h4">
+                        <div className="list-inline-item col-3 m-0 p-0">
+                            Node
+                        </div>
+                        <div className="list-inline-item col-3 m-0 p-0">
+                            CSS
+                        </div>
+                        <div className="list-inline-item col-3 m-0 p-0">
+                            React
+                        </div>
+                        <div className="list-inline-item col-3 m-0 p-0">
+                            Php
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
     return(
         <div className="container-fluid position-relative">
             <div className="row m-0">
@@ -7,6 +63,11 @@ export default function Projects(){
                         <div className="hr-line mb-3"></div>
                         •
                         Projects
+                    </div>
+                    <div className="row">
+                        {
+                            samples.map(sample => createPortfolioCardTemplate(sample.title, sample.description, sample.image))
+                        }
                     </div>
                 </div>
             </div>
