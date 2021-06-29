a35c8d07d0db31b88ec147148a305b75b9b6485e
Neo.Eskini <mreskini30@gmail.com>
2021-06-29 14:30:37 +0430

    completed the technologies cards only for desktop view

--- src/components/technologies.jsx
+++ src/components/technologies.jsx
@@ -1,3 +1,4 @@
+import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
 export default function Technologies(){
     return(
         <div className="container-fluid position-relative">
@@ -10,6 +11,43 @@ export default function Technologies(){
                     </div>
                     <div className="h3 font-weight-light text-secondary col-lg-10">
                         I've worked with a range a technologies in the web development world. From Back-end To Design
+                    </div>
+                    <div className="mt-5 row m-0">
+                        <div className="col-lg-3 p-3 bg-secondary-color shadow-light rounded">
+                            <DiReact size="3rem" className="text-light mb-3" />
+                            <div className="text-light h2">
+                                Front-End
+                            </div>
+                            <div className="text-light h4 font-weight-light">
+                                Experience with
+                                <br />
+                                React.js
+                            </div>
+                        </div>
+                        <div className="col-lg-1"></div>
+                        <div className="col-lg-3 p-3 bg-secondary-color shadow-light rounded">
+                            <DiFirebase size="3rem" className="text-light mb-3" />
+                            <div className="text-light h2">
+                                Back-End
+                            </div>
+                            <div className="text-light h4 font-weight-light">
+                                Experience with
+                                <br />
+                                Node and Databases
+                            </div>
+                        </div>
+                        <div className="col-lg-1"></div>
+                        <div className="col-lg-3 p-3 bg-secondary-color shadow-light rounded">
+                            <DiZend  size="3rem" className="text-light mb-3" />
+                            <div className="text-light h2">
+                                UI/UX
+                            </div>
+                            <div className="text-light h4 font-weight-light">
+                                Experience with
+                                <br />
+                                Tools like Figma
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
